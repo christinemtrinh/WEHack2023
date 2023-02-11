01816f23f51e5e2935b34c8d5b49bf523799adf8
Christine Trinh <96509171+christinemtrinh@users.noreply.github.com>
2023-02-11 15:22:38 -0600

    Sign on (Not yet functional)

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    uname = StringField('Username', validators=[DataRequired()])
+    pswd = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
