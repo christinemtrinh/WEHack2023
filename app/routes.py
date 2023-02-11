c2aa9fd1b1fa4319a7eb46370a1053b923fbb05c
Christine Trinh <96509171+christinemtrinh@users.noreply.github.com>
2023-02-11 15:40:46 -0600

    setting up sqllite

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,11 @@ def index():
 	user = {'username': 'Miguel'}
 	return render_template('index.html', title='Home', user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login(): 
 	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(
+			form.uname.data, form.remember_me.data))
+		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign In', form=form)
